52a7c53a5efa1ebbb7668ad051e3e3f17c6157cf
Roza Chaikozova <r.chaikozova@gmail.com>
2021-03-19 18:41:20 +0600

    for heroku

--- geek/settings.py
+++ geek/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'events',
     'courses',
+    'shortuuid',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-import shortuuid
+#import shortuuid
 from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import ValidationError
@@ -86,22 +86,22 @@ class UserRetrieveUpdateDeleteAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["PATCH"])
-def reset_user_password(request):
-    """
-    Changing user password
-    """
-
-    user_id = request.data.get('user_id', None)
-    if not user_id:
-        return Response({"error": "Need users id to reset password"}, status=status.HTTP_400_BAD_REQUEST)
-
-    user = get_object_or_404(User, pk=user_id)
-    password = str(shortuuid.uuid())[:8]
-    password2 = str(shortuuid.uuid())[:8]
-    if password != password2:
-        raise ValidationError({'password': 'Passwords must match'})
-    else:
-        user.set_password(password)
-        user.save()
-        return Response({"message": "successfuly updated user password"}, status=status.HTTP_200_OK)
+# @api_view(["PATCH"])
+# def reset_user_password(request):
+#     """
+#     Changing user password
+#     """
+#
+#     user_id = request.data.get('user_id', None)
+#     if not user_id:
+#         return Response({"error": "Need users id to reset password"}, status=status.HTTP_400_BAD_REQUEST)
+#
+#     user = get_object_or_404(User, pk=user_id)
+#     password = str(shortuuid.uuid())[:8]
+#     password2 = str(shortuuid.uuid())[:8]
+#     if password != password2:
+#         raise ValidationError({'password': 'Passwords must match'})
+#     else:
+#         user.set_password(password)
+#         user.save()
+#         return Response({"message": "successfuly updated user password"}, status=status.HTTP_200_OK)
