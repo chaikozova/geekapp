9624045b31dbb6c5a793851a8d780391bc0c1db2
altynaibshbv <altysha.beishebaeva@gmail.com>
2021-03-19 21:41:16 +0600

    making requests

--- users/views.py
+++ users/views.py
@@ -1,15 +1,15 @@
 from django.http import Http404
 
 
-from .serializers import UserRegistrationSerializer, LoginSerializer,\
-    UserListSerializer, UserRetrieveUpdateDeleteSerializer
+from .serializers import UserRegistrationSerializer, LoginSerializer, \
+    UserListSerializer, UserRetrieveUpdateDeleteSerializer, RequestSerializer
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import User
+from .models import User, Request
 
 
 class LoginView(APIView):
@@ -81,3 +81,12 @@ class UserRetrieveUpdateDeleteAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RequestAPIView(generics.GenericAPIView,
+                     mixins.CreateModelMixin):
+    serializer_class = RequestSerializer
+    queryset = Request.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
