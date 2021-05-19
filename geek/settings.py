7149d21e6dbfbbdf8195eb0b81e99364d4bcebd3
chaikozova <r.chaikozova@gmail.com>
2021-05-19 13:40:00 +0600

    static root

--- geek/settings.py
+++ geek/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/root/geekapp/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
